5ab9f6eb2e0a1a8317690d6057f4941e9e5b687a
Сенин Роман <rastopyr@gmail.com>
2017-06-05 23:20:20 +0300

    Add blueprint for csp-styles

---
+++ blueprints/ember-cli-simple-grid/index.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = {
+  description: '',
+
+  afterInstall() {
+    return this.addPackageToProject('ember-cli-csp-style');
+  }
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+};
